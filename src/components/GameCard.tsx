5f99a08ee72c05e5cf9b5ecdba266ab2616bab56
Frank <yoinspiration@outlook.com>
2024-07-04 14:54:07 +0800

    Building game cards

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,20 @@
+import {Game} from "../hooks/useGames.ts";
+import {Card, CardBody, Heading, Image} from "@chakra-ui/react";
+
+interface Props {
+  game: Game
+}
+
+const GameCard = ({game}: Props) => {
+  return <Card borderRadius="10px" overflow="hidden">
+    <Image
+      src={game.background_image}
+      alt=''
+    />
+    <CardBody>
+      <Heading fontSize='2xl'>{game.name}</Heading>
+    </CardBody>
+  </Card>;
+};
+
+export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,19 @@
-import { Text } from "@chakra-ui/react";
+import {SimpleGrid, Text} from "@chakra-ui/react";
 import useGames from "../hooks/useGames.ts";
+import GameCard from "./GameCard.tsx";
 
 const GameGrid = () => {
   const {games, error} = useGames()
 
   if (error) return <Text>{error}</Text>;
   return (
-    <ul>
+    <SimpleGrid columns={{
+      sm: 1, md: 2, lg: 3, xl: 4
+    }} padding='10px' spacing={10}>
       {games.map((game) => (
-        <li key={game.id}>{game.name}</li>
+        <GameCard key={game.id} game={game} />
       ))}
-    </ul>
+    </SimpleGrid>
   );
 };
 
