a838ca94e267eb921b6efbd486ad2208b1c878a1
Frank <yoinspiration@outlook.com>
2024-07-05 19:52:20 +0800

    添加了TODO，并删掉了重复的代码

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,12 +1,6 @@
 // import useData from "./useData.ts";
 import platforms from "../data/platforms.ts";
 
-// interface Platform {
-//   id: number;
-//   name: string;
-//   slug: string;
-// }
-
 // const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
 const usePlatforms = () => ({data: platforms, isLoading: false, error: null});
 
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
+    // TODO: hide this sensitive information
     key: "feedbf1896ec4551a0c3491f58650b16",
   },
 });
