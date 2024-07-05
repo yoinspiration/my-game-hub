17d2a86ba7165dd4cf737070e6a4d0ddb37cb716
Frank <yoinspiration@outlook.com>
2024-07-05 13:36:36 +0800

    Refactor: Extract a query object

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,19 +1,18 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames, {Platform} from "../hooks/useGames.ts";
+import {SimpleGrid, Text} from "@chakra-ui/react";
+import useGames from "../hooks/useGames.ts";
 import GameCard from "./GameCard.tsx";
 import GameCardSkeleton from "./GameCardSkeleton.tsx";
 import GameCardContainer from "./GameCardContainer.tsx";
-import {Genre} from "../hooks/useGenres.ts";
+import {GameQuery} from "../App.tsx";
 
 interface Props {
-  selectedGenre: Genre | null;
-  selectedPlatform: Platform | null;
+  gameQuery: GameQuery
 }
 
-const GameGrid = ({selectedGenre, selectedPlatform}: Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
+const GameGrid = ({gameQuery}: Props) => {
+  const {data, error, isLoading} = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
-
+  
   if (error) return <Text>{error}</Text>;
   return (
     <SimpleGrid
@@ -29,12 +28,12 @@ const GameGrid = ({selectedGenre, selectedPlatform}: Props) => {
       {isLoading &&
         skeletons.map((skeleton) => (
           <GameCardContainer key={skeleton}>
-            <GameCardSkeleton />
+            <GameCardSkeleton/>
           </GameCardContainer>
         ))}
       {data.map((game) => (
         <GameCardContainer key={game.id}>
-          <GameCard game={game} />
+          <GameCard game={game}/>
         </GameCardContainer>
       ))}
     </SimpleGrid>
