a7951147039aa6f09b41dd2062a0ed40d9c30369
Frank <yoinspiration@outlook.com>
2024-07-06 12:23:19 +0800

    Implement infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,7 @@
 import { GameQuery } from "../App.tsx";
-import { useQuery } from "@tanstack/react-query";
-import games from "../data/games.ts";
+import {useInfiniteQuery} from "@tanstack/react-query";
 import {Platform} from "./usePlatforms.ts";
-import APIClient from "../services/api-client.ts";
+import APIClient, {FetchResponse} from "../services/api-client.ts";
 
 export interface Game {
   id: number;
@@ -16,18 +15,22 @@ export interface Game {
 const apiClient = new APIClient<Game>("/games")
 
 const useGames = (gameQuery: GameQuery) =>
-  useQuery({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
-    queryFn: () => apiClient.getAll({
+    queryFn: ({pageParam}) => apiClient.getAll({
       params: {
         genres: gameQuery.genre?.id,
         parent_platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
         search: gameQuery.searchText,
+        page: pageParam
       },
     }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined
+    },
     staleTime: 24 * 60 * 60 * 1000, // 24h
-    initialData: { count: games.length, results: games },
   });
 
 export default useGames;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
-import platforms from "../data/platforms.ts";
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client.ts";
+import platforms from "../data/platforms.ts";
 
 export interface Platform {
   id: number;
@@ -15,7 +15,7 @@ const usePlatforms = () =>
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24h
-    initialData: { count: platforms.length, results: platforms },
+    initialData: platforms,
   });
 
 export default usePlatforms;
